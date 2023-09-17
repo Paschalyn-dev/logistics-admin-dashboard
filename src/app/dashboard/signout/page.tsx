767a94c5ec89f9a2b57a4a98188e571a6382c7cb
dareadewole <dare.adewole@artsplit.com>
2023-09-17 22:41:37 +0100

    chore: use client

--- src/app/dashboard/signout/page.tsx
+++ src/app/dashboard/signout/page.tsx
@@ -1,26 +1,30 @@
+'use client';
+
 import { useRouter } from "next/navigation";
 import { logout } from "../services/libs/staff-auth";
+import { customerLogout } from "../services/libs/customer-auth";
 import { useStaffLogin } from "../services/swr-functions/staff-swr";
-import { useEffect } from "react";
 import Loader from "../services/Loader/spinner";
+import { useCustomerLogin } from "../services/swr-functions/customer-swr";
 
-export default function SignOut({staffLogin}: any){
+export default function SignOut({staffLogin, customerLogin}: any){
     const {mutate, loading, loggedOut} = useStaffLogin(staffLogin || '');
+    const {mutate: m, loading: l, loggedOut: lO} = useCustomerLogin(customerLogin || '');
     const router = useRouter();
 
-    useEffect(() => {
-        if(loggedOut){
-           router.replace('/');
-           console.log('router')
-        }
-    }, [loggedOut]);
-    if(loggedOut) return <Loader />
-    if(loading) return <Loader />
+    if(loggedOut || lO){
+        console.log('router')
+        router.replace('/');
+    }
+    // if(loggedOut) return <Loader />
+    if(loading || l) return <Loader />
     return(
         <button 
         onClick={() => {
             logout();
+            customerLogout();
             mutate(null);
+            m(null);
         }}
         >Sign Out</button>
     )
