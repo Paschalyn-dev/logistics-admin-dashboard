af60b769829e1fc2592095946c9e1cba7fce58bf
dareadewole <dare.adewole@artsplit.com>
2023-11-20 01:11:40 +0100

    Shipment

--- src/app/dashboard/context/context.tsx
+++ src/app/dashboard/context/context.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { parsedUrlQueryToParams } from "next/dist/server/future/route-matches/route-match";
+// import { parsedUrlQueryToParams } from "next/dist/server/future/route-matches/route-match";
 import {createContext, useState} from "react";
 export const phoneRegExp = /^[+]?((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 // export const phoneRegExp = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
